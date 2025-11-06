8f3b784c56cc2ab455afd064ac7b3ce1b74f1111
Prasad211967 <kingkeliven17@gmail.com>
2025-11-06 11:36:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 React App Dockerized via GitHub Actions!</h1>
+      <p>This is React Docker Demo 6 - built and pushed automatically to Docker Hub 🎉</p>
+    </div>
+  );
+}
+
+export default App;
